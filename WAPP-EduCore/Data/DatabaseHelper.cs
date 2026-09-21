8b18c8202844035c092b81806922db363982ae6b
MingCyn <joslyn.cyn05@gmail.com>
2026-09-21 23:16:44 +0800

    Configure Supabase PostgreSQL connection

---
+++ WAPP-EduCore/Data/DatabaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using Npgsql;
+
+namespace WAPP_EduCore.Data
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string connectionString =
+            ConfigurationManager.ConnectionStrings["SupabasePostgres"].ConnectionString;
+
+        public static NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
